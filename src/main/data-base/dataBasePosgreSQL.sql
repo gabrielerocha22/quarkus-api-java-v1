cc5d43c72e34d9a6abde8a4a301207ad86b8be4d
Gabriele Rocha <gabrielerocha17@outlook.com>
2022-09-09 12:09:01 -0300

    database modification

---
+++ src/main/data-base/dataBasePosgreSQL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE quarkusDocker;
+CREATE TABLE Customer(
+	id bigint not null
+	age varchar(255), 
+	email varchar(255), 
+	name varchar(255), 
+	primary key (id)
+	)
--- src/main/resources/scriptSQL.sql
+++
@@ -1,8 +0,0 @@
-create sequence hibernate_sequence start with 1 increment by 1;
-create table Customer (id bigint not null, age varchar(255), email varchar(255), name varchar(255), primary key (id));
-insert into Customer (id, name, email, age) values (1, 'Gabi', 'gabi@hotmail.com', 21);
-insert into Customer (id, name, email, age) values (2, 'Teste', 'teste2@hotmil.com', 45);
-insert into Customer (id, name, email, age) values (3, 'Teste3', 'ssssssss@hotmil.com', 87);
-insert into Customer (id, name, email, age) values (4, 'Teste4', 'abcd@hotmil.com', 54);
-insert into Customer (id, name, email, age) values (5, 'Teste5', 'abcd@hotmil.com', 16);
-insert into Customer (id, name, email, age) values (6, 'Teste6', 'bbbbb@ccccccc.com', 12);
