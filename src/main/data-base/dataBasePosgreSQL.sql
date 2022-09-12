918ac6fcae991d4ece70b5f44f298004e42f9173
Gabriele Rocha <gabrielerocha17@outlook.com>
2022-09-12 15:37:04 -0300

    add docker-compose

--- src/main/data-base/dataBasePosgreSQL.sql
+++ src/main/data-base/dataBasePosgreSQL.sql
@@ -6,3 +6,8 @@ CREATE TABLE Customer(
 	name varchar(255), 
 	primary key (id)
 	)
+
+INSERT INTO Customer VALUES
+("2","exemplo1@hotmail.com", "exemplo", 1),
+("45","email@hotmail.com", "gabi", 2),
+("12","jose@hotmail.com", "jose", 3)
